54416c4bc6d7969b00ed95e7d196525929e6d634
Daniele Gargano <daniele.gargano.office@gmail.com>
2026-06-19 11:47:16 +0200

    test: add matchMedia mock for jsdom

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -8,4 +8,17 @@ declare const process: {
 process.env.TZ = 'UTC';
 import '@testing-library/jest-dom';
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    addListener: () => {},
+    removeListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
 
